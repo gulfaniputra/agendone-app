11ed512f85603e2e436fd5e403fcd1288cd9c97d
Gulfani Putra <gulfaniputra@gmail.com>
2024-04-20 15:34:06 +0800

    Create basic UI with vanilla HTML

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
 import "./styles.css";
 
 export default function App() {
-  return "Agendone";
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Task</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Agendone</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Task #1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>{" "}
+        <li>
+          <label>
+            <input type="checkbox" />
+            Task #2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>{" "}
+        <li>
+          <label>
+            <input type="checkbox" />
+            Task #3
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
 }
